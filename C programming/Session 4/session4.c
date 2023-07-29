ca2a162e75ad6565e3b95d0e76c70576405ba5f2
Mariam Mahmoud <mariammahmoud88779@gmail.com>
2023-07-29 16:09:46 +0300

    add files

--- C programming/Session 4/session4.c
+++ C programming/Session 4/session4.c
@@ -7,20 +7,59 @@ for(int i=0 ; i<10 ; ++i)
     printf("%d", x);
 }
 */
+ 
+//for looping -> printing stars 
 
 /* getting and scanning number of rows*/
-int rows;
+
+/*int rows;
 printf("Enter the number of rows\n");
 scanf("%d", &rows);
 
-/* looping */
-for(int i=0 ; i<rows ; ++i)
+/* looping -> better to initialize before for*/
+
+/*for(int i=0 ; i<rows ; ++i)
 {
     printf("*****\n");
-}
+}*/
 
+// a loop for rows and a loop for stars number by user x nfso
 
+/*getting number of rows and stars from user*/
+/*int row_star;
+printf("enter number of both rows and stars\n");
+scanf("%d", &row_star);
+/*row counter*/
+/*int r;
+/*star counter*/
+/*int s;
+/*looping*/
+/*for (r=0 ; r<row_star ; ++r)
+{
+    for(s=0 ; s<row_star ; ++s)
+    {
+        printf("*");
+    }
+    printf(" \n ");
+}*/
 
+// pyramid
+/*int x;
+printf("Enter the number");
+scanf("%d", &x);
+int i;
+int u;
+for(u=1 ; i<=x ; ++i)
+{
+    for(i=1 ; u<=i ; ++u)
+    {
+        printf("*\n");
+    }
+}*/
+/*while(condition);
+{
+    code;
+}*/
 
 return 0;    
 }
