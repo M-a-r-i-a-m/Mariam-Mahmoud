11ca27e752248ab020cdd96aa1aa2cdb2645b05b
Mariam Mahmoud <mariammahmoud88779@gmail.com>
2023-08-05 19:50:47 +0300

    add files

---
+++ C programming/Session 6/std_types.h
@@ -0,0 +1,8 @@
+typedef unsigned char uint8;
+typedef char sint8;
+typedef unsigned short int uint16;
+typedef signed short int sint16;
+typedef unsigned long int uint32;
+typedef signed long int sint32;
+typedef float float32;
+typedef double float64;
